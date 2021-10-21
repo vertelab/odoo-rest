6b43a906c26a076bc0fbe7af482fc73a1f0d5989
Daniel Eriksson <daniel.eriksson@vertel.se>
2021-10-21 13:59:12 +0000

    rest_unit4bw: bugfixes

--- rest_unit4bw/models/rest_api.py
+++ rest_unit4bw/models/rest_api.py
@@ -14,16 +14,15 @@ class RestApiAgresso(models.Model):
         if self == self.env.ref("rest_unit4bw.api_unit4bw"):
             res = self.call_endpoint(method="GET", endpoint_url="/v1/status")
             _logger.warning(res)
-            if res.get('code') == "1030":
+            if "1030" in res.get('response', {}):
                 raise UserError(_("Connection is working"))
             else:
                 raise UserError(_("The connection is not working"))
         else:
             return super(RestApiAgresso, self).test_connection()
 
-    def get_projects(self):
-        res = self.call_endpoint(method="GET", endpoint_url="/v1/objects/projects")
-        _logger.warning(res)
+    # def get_projects(self):
+    #     res = self.call_endpoint(method="GET", endpoint_url="/v1/objects/projects")
 
     def get_project(self, project):
         project_nr = "UV-2021-1"
