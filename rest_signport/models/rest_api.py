6eb28683716e3a5321963c0beec99f259a2155a1
victor ekstrom <victor.ekstrom@vertel.se>
2021-10-21 11:47:07 +0000

    rest_signport: temporary

--- rest_signport/models/rest_api.py
+++ rest_signport/models/rest_api.py
@@ -2,6 +2,7 @@
 
 import logging
 from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -14,3 +15,15 @@ class RestApiAgresso(models.Model):
 
     def signport_complete_signing(self):
         pass
+
+    def test_connection(self):
+        if self == self.env.ref("rest_signport.api_signport"):
+            res = self.call_endpoint(method="get", endpoint_url="/__Health", headers={"accept": "application/json"})
+            _logger.warning("response"*99)
+            _logger.warning(res)
+            if res.get('code') == "200":
+                raise UserError(_("Connection is working"))
+            else:
+                raise UserError(_("The connection is not working"))
+        else:
+            return super(RestApiAgresso, self).test_connection()
