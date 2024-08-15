2a0c7e72216b16ba9c86df68b06bf6bbe29fd5d8
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-15 10:55:32 +0200

    Update __manifest__.py

--- rest_base/__manifest__.py
+++ rest_base/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,8 +21,8 @@
 
 {
     'name': 'Rest: Base',
-    'version': '14.0.1.4.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.4.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Implements a standardized way to call REST APIs from Odoo and logs errors.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
